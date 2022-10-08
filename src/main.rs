5a18333656ddcb8f450ce30e42367f9fff8e4125
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-10-08 19:08:58 -0400

    Creating registers

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,22 @@ fn main() {
     let mut conn = PgConnection::establish(&db_url).expect("No nos pudimos conectar a la base de datos");
     
     // Indicamos que vamos a usar el esquema Post y el modelo
-    use self::models::Post;
+    use self::models::{Post, NewPost};
     use self::schema::posts::dsl::*;
+    use self::schema::posts;
+
+    let new_post = NewPost {
+        title: "Go vs. Rust",
+        body:  "Acompañanos en el siguiente articulo, donde aprenderemos los pros y contras de estos maravillosos lenguajes",
+        slug:  "post-technical",
+    };
+    // Insertamos los registros
+    let post: Post = diesel::insert_into(posts::table).values(new_post).get_result(&mut conn).expect("La insercion de datos a fallado");
 
     //Realizamos la consulta Select * from posts
-    let post_result = posts.load::<Post>(&mut conn).expect("Error al ejecutar la query");
+    //let post_result = posts.load::<Post>(&mut conn).expect("Error al ejecutar la query");
+    //Realizamos la consulta Select * from posts 1
+    let post_result = posts.limit(1).load::<Post>(&mut conn).expect("Error al ejecutar la query");
 
     // Iteramos sobre la consulta
     for post in post_result {
