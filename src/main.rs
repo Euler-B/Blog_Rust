f605b2a39bc08a75f54d80b64190a07d69b876f8
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-10-08 18:29:39 -0400

    create models(tables)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate diesel;
+
+pub mod schema;
+pub mod models;
+
+use dotenvy::dotenv;
+use std::env;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
 fn main() {
-    println!("Hello, world!");
+    // Lectura de variable de entorno
+    dotenv().ok();
+    let db_url     = env::var("DATABASE_URL").expect("db url variable no encontrada");  
+    
+    // Conexion a la base de datos 
+    let mut conn = PgConnection::establish(&db_url).expect("No nos pudimos conectar a la base de datos");
+    
+    // Indicamos que vamos a usar el esquema Post y el modelo
+    use self::models::Post;
+    use self::schema::posts::dsl::*;
+
+    //Realizamos la consulta Select * from posts
+    let post_result = posts.load::<Post>(&mut conn).expect("Error al ejecutar la query");
+
+    // Iteramos sobre la consulta
+    for post in post_result {
+        println!("{}", post.title);
+    }
 }
